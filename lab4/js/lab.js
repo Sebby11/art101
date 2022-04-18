e43e48d3259ac5bef6a2f0f2f41bc4d458d70b96
Sebby11 <used4sebastianid@gmail.com>
2022-04-18 16:34:03 -0700

    lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,22 @@
+// [Summary] Programs movement for character within video game.
+// [Description]When character movement changes the
+// 		animation frames changes will due to if statements and loops.
+// @link   https://sebby11.github.io/art101/lab4/  https://Albmedin.github.io/art101/lab4/
+// @file   File programs character movement for game.
+// @author Alexa Medina & Sebastian Medina
+// @since  0.0.1
+
+
+// [Note: Frames drawn outside program]
+// Import jumping animation frames
+// Import walking animation frames
+// Import idle animation frames
+// If character idle
+	// Loop through each idle frame until character does other movement
+		// Show idle frame
+// else if character walking
+	// Loop through each walking frame until character does other movement
+		// Show walking frame
+// else if character jumping
+	// Loop through jumping frame until character does other movement
+		// Show jumping frame 
