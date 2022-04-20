d264c626cf6b1460d5effc7340f47493050a5987
Sebby11 <used4sebastianid@gmail.com>
2022-04-20 15:45:32 -0700

    lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,23 @@
+/*
+* Authors: Alexa Medina <albmedina@ucsc.edu> & Sebastian Medina <seamedin@ucsc.edu>
+* Created: 20 April
+* License: Public Domain
+*/
+
+
+// Variable Declaration
+var make  = "Honda";
+var model = "CR-V";
+var color = "Silver";
+var year  = 2005;
+
+
+// Calculate
+var age = 2022 - year;
+
+// Output to HTML
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
